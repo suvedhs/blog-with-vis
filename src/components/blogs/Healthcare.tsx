99709ce3a1d032ff6dbbb08082f9ef78c4437d9e
suvedhs <suvedhs@gmail.com>
2022-04-05 00:49:39 -0500

    fixed all eslint

--- src/components/blogs/Healthcare.tsx
+++ src/components/blogs/Healthcare.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react'
 
-function Healthcare() {
+function Healthcare () {
   return (
     <div className="about">
       <div>
@@ -15,7 +15,7 @@ function Healthcare() {
             <h1>About</h1>
             <p>
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
+              industry. Lorem Ipsum has been the standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
@@ -23,7 +23,7 @@ function Healthcare() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Healthcare;
+export default Healthcare
