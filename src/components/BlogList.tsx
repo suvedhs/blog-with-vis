0eb6cdac66f1a178e9d9e82647c957a106117bbc
suvedhs <suvedhs@gmail.com>
2022-08-16 14:36:46 -0500

    finished showing blog cards

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -4,7 +4,7 @@ import '../index.css'
 
 const Container = styled.div`
     scroll-margin-top: 10em;
-    height: 500vh;
+    height: 400vh;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -36,77 +36,86 @@ const SubHeader = styled.h3`
     will-change: font-size;
 `
 
-// const BlogBox = styled.a`
-//     padding: 30px;
-//     display:flex;
-//     flex-direction: column;
-//     align-content: center;
-//     justify-content: center;
+const RealList = styled.div`
+    background-color: #88f;
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    bottom: 0;
+    z-index: 2;
+`
 
-//     img {
-//         height: 10em;
-//         border-radius: 5px;
-//     }
+const BlogBox = styled.a`
+    padding: 30px;
+    display:flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
 
-//     img.static {
-//         position:absolute;
-//         top: 30px;
-//     }
+    img {
+        height: 10em;
+        border-radius: 5px;
+    }
 
-//     :hover img.static {
-//         opacity: 0;
-//     }
+    img.static {
+        position:absolute;
+        top: 30px;
+    }
 
-//     p {
-//         text-align: center;
-//         font-size: 30px;
-//         margin-bottom: 0;
-//     }
-// `
+    :hover img.static {
+        opacity: 0;
+    }
 
-// const Ul = styled.ul`
-//     display: flex;
-//     flex-direction: row;
-//     -webkit-overflow-scrolling: touch;
-//     overflow: scroll;
-//     max-width: 95%;
-//     height: auto;
-//     align-items: center;
-//     justify-content: start;
-// `
+    p {
+        text-align: center;
+        font-size: 30px;
+        margin-bottom: 0;
+    }
+`
 
-// const Li = styled.li`
-//   -webkit-box-align: center;
-//   -webkit-box-pack: center;
-//   -webkit-tap-highlight-color: transparent;
-//   align-items: center;
-//   height: 100%;
-//   justify-content: center;
-//   text-decoration: none;
-//   -webkit-box-align: center;
-//   -webkit-box-pack: center;
-//   position: relative;
-//   left: 0;
-//   -webkit-tap-highlight-color: transparent;
-//   align-items: center;
-//   display: flex;
-//   justify-content: center;
-//   margin: 5% 40px;
-//   text-decoration: none;
-//   white-space: nowrap;
-//   `
+const Ul = styled.ul`
+    display: flex;
+    flex-direction: row;
+    -webkit-overflow-scrolling: touch;
+    overflow: scroll;
+    max-width: 95%;
+    height: 100vh;
+    align-items: center;
+    justify-content: start;
+`
+
+const Li = styled.li`
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  -webkit-tap-highlight-color: transparent;
+  align-items: center;
+  height: 40%;
+  justify-content: center;
+  text-decoration: none;
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  position: relative;
+  left: 0;
+  -webkit-tap-highlight-color: transparent;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin: 5% 40px;
+  text-decoration: none;
+  white-space: nowrap;
+  `
 
 const BlogList = (props: {topics: {name: string; to: string; img: string, animation: string}[] }) => {
-//   const { topics } = props
-//   const Blogs: any = () => topics.map((link: {name: string, to: string, img:string, animation: string }) =>
-//     <Li className='shadow point' key={link.name}>
-//         <BlogBox className='small rounded link' href={link.to}>
-//             <img className='static' src={require('../assets/' + link.img)} alt= {link.img + ' image'}></img>
-//             <img className='active' src={require('../assets/' + link.animation)} alt= {link.img + ' image'}></img>
+  const { topics } = props
+  const Blogs: any = () => topics.map((link: {name: string, to: string, img:string, animation: string }) =>
+    <Li className='shadow orange point' key={link.name}>
+        <BlogBox className='small rounded link' href={link.to}>
+            <img className='static' src={require('../assets/' + link.img)} alt= {link.img + ' image'}></img>
+            <img className='active' src={require('../assets/' + link.animation)} alt= {link.img + ' image'}></img>
 
-  //             <p>{link.name}</p>
-  //         </BlogBox>
-  //     </Li>)
+              <p>{link.name}</p>
+          </BlogBox>
+      </Li>)
 
   const useEffectRef = useRef(false)
   const topicsRef = useRef<HTMLDivElement>(null)
@@ -119,8 +128,8 @@ const BlogList = (props: {topics: {name: string; to: string; img: string, animat
     window.addEventListener('scroll', () => {
       if (topicsRef.current) {
         const scrollPosition = topicsRef.current.getBoundingClientRect().top
-        console.log(scrollPosition / (window.innerHeight * 4))
-        const frozen = scrollPosition <= 0 && window.innerHeight * 4 + scrollPosition > 0
+        const frozen = scrollPosition <= 0 && window.innerHeight * 3 + scrollPosition > 0
+        console.log(scrollPosition + window.innerHeight * 3)
         if (topicsWrapperRef.current && headRef.current && subHeadRef.current) {
           if (frozen) {
             requestAnimationFrame(() => {
@@ -152,9 +161,11 @@ const BlogList = (props: {topics: {name: string; to: string; img: string, animat
                 <Head ref={headRef}>Topics</Head>
                 <SubHeader ref={subHeadRef} className='small'>Hold on tight and check out my work.</SubHeader>
             </TopicsWrapper>
-            {/* <Ul>
-                <Blogs />
-            </Ul> */}
+            <RealList>
+              <Ul>
+                  <Blogs />
+              </Ul>
+            </RealList>
         </Container>
     </>
   )
